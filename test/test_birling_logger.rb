cb5f491bb50cd73749c43f26f1078a9a65f97a57
Scott Tadman <tadman@postageapp.com>
2021-02-15 23:54:34 -0500

    Fix StringIO dependency check

--- test/test_birling_logger.rb
+++ test/test_birling_logger.rb
@@ -1,30 +1,31 @@
-require File.expand_path('helper', File.dirname(__FILE__))
+require File.expand_path('helper', __dir__)
+require 'stringio'
 
 class TestBirlingLogger < Test::Unit::TestCase
   def test_defaults
     temp_path do |path|
       log = Birling::Logger.new(path)
-    
+
       assert log
 
       assert log.opened?
       assert !log.closed?
       assert_equal 0, log.size
       assert Time.now >= log.create_time
-      
+
       assert_equal Birling::Formatter, log.formatter
       assert_equal Time, log.time_source
       assert_equal nil, log.period
     end
   end
-  
+
   def test_with_handle
     s = StringIO.new
-    
+
     log = Birling::Logger.new(s)
-    
+
     assert log.opened?
-    
+
     assert_equal 0, log.size
   end
 
@@ -34,9 +35,9 @@ class TestBirlingLogger < Test::Unit::TestCase
     buffer = StringIO.new
 
     log = Birling::Logger.new(buffer, time_source: Time::Warped)
-    
+
     assert log.opened?
-    
+
     assert_equal 0, log.size
 
     $stdout = log
@@ -56,21 +57,21 @@ class TestBirlingLogger < Test::Unit::TestCase
   ensure
     $stdout = stdout
   end
-  
+
   def test_formatter
     formatter_called = false
     formatter = lambda do |severity, time, program, message|
       formatter_called = true
       message
     end
-    
+
     output = StringIO.new
     log = Birling::Logger.new(output, formatter: formatter)
-    
+
     log.debug("Test")
-    
+
     assert_equal true, formatter_called
-    
+
     output.rewind
     assert_equal "Test", output.read
   end
@@ -78,44 +79,44 @@ class TestBirlingLogger < Test::Unit::TestCase
   def test_default_level
     temp_path do |path|
       log = Birling::Logger.new(path)
-    
+
       assert log
 
       assert log.opened?
       assert !log.closed?
       assert_equal 0, log.size
-      
+
       assert log.debug?
 
       log.debug("Test")
-      
+
       current_size = log.size
       assert current_size > 0
     end
   end
-  
+
   def test_direct_write
     output = StringIO.new
-  
+
     log = Birling::Logger.new(output)
-    
+
     log << "TEST"
-    
+
     assert_equal "TEST", output.string
   end
-  
+
   def test_level_filter
     output = StringIO.new
-  
+
     log = Birling::Logger.new(
       output,
       formatter: lambda { |s, t, p, m| "#{m}\n" },
       severity: :info
     )
-    
+
     log.debug("DEBUG")
     log.info("INFO")
-    
+
     output.rewind
     assert_equal "INFO\n", output.read
   end
@@ -127,67 +128,67 @@ class TestBirlingLogger < Test::Unit::TestCase
       assert log.debug?
 
       log.debug("Test")
-      
+
       current_size = log.size
       assert current_size > 0
-      
+
       create_time = log.create_time
       assert create_time <= Time.now
-      
+
       log.close
-      
+
       log = Birling::Logger.new(path)
-      
+
       assert_equal current_size, log.size
       assert_equal create_time, log.create_time
     end
   end
-  
+
   def test_time_source
     temp_path do |path|
       frozen_time = Time.now
       Time::Warped.now = frozen_time
-      
+
       logger = Birling::Logger.new(path, time_source: Time::Warped)
-      
+
       assert_equal frozen_time, logger.time_source.now
     end
   end
-  
+
   def test_cycling
     temp_path(:cycle) do |path|
       start = Time.now
       Time::Warped.now = start
       logger = Birling::Logger.new(path, period: 1, time_source: Time::Warped)
-      
+
       assert_equal 1, logger.period
-      
+
       current_path = logger.current_path
       assert_equal '%s', logger.path_time_format
-    
+
       logger.debug("Test")
-      
+
       Time::Warped.now += 1
-      
+
       logger.debug("Test")
 
       assert_not_equal current_path, logger.current_path
-      
+
       current_path = logger.current_path
 
       Time::Warped.now += 1
-      
+
       logger.debug("Test")
-      
+
       assert_not_equal current_path, logger.current_path
     end
   end
-  
+
   def test_retain_count
     temp_path(:cycle) do |path|
       start = Time.now
       Time::Warped.now = start
-      
+
       retain_count = 10
 
       logger = Birling::Logger.new(
@@ -204,23 +205,23 @@ class TestBirlingLogger < Test::Unit::TestCase
 
         logger.debug("Test")
       end
-      
+
       assert_equal retain_count, Dir.glob(logger.path_format % '*').length
     end
   end
-  
+
   def test_default_formatter
     temp_path(:cycle) do |path|
       logger = Birling::Logger.new(path)
-      
+
       lines = 100
-      
+
       lines.times do
         logger.debug("Test")
       end
-      
+
       logger.close
-      
+
       assert_equal lines, File.readlines(path).length
     end
   end
@@ -228,13 +229,13 @@ class TestBirlingLogger < Test::Unit::TestCase
   def test_retain_period
     temp_path(:cycle) do |path|
       retain_period = 3
-      
+
       logger = Birling::Logger.new(
         path,
         period: 1,
         retain_period: retain_period
       )
-      
+
       assert_equal true, File.exist?(path)
       assert_equal true, File.symlink?(path)
 
@@ -245,7 +246,7 @@ class TestBirlingLogger < Test::Unit::TestCase
         Time::Warped.now += 1
         logger.debug("Test")
       end
-      
+
       assert_equal retain_period + 1, Dir.glob(logger.path_format % '*').length
     end
   end
@@ -257,6 +258,6 @@ class TestBirlingLogger < Test::Unit::TestCase
       invalid = (0..255).to_a.pack('C*')
 
       logger.debug(invalid)
-    end    
+    end
   end
 end
